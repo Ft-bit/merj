0003c15cb526f306a8c5398b0bef67848e013eb3
Festus.net <fredbv22@gmail.com>
2026-05-18 22:34:56 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'd8j0ntlcm91z4.cloudfront.net' },
+      { protocol: 'https', hostname: '**' },
+    ],
+  },
 }
 module.exports = nextConfig
