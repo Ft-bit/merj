9151dbb912c16b77654efec073f52722664a76eb
Ft.dev <fredbv22@gmail.com>
2026-07-11 11:57:28 +0100

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,17 @@
+import { AuthProvider } from '../context/AuthContext'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, padding: 0, background: '#000', color: '#fff' }}>
+        <AuthProvider>
+          {children}
+        </AuthProvider>
+      </body>
+    </html>
+  )
+}
