a3e057b0b16d43e5fc4df6ca489196fb7bf89c95
Ft.dev <fredbv22@gmail.com>
2026-07-13 19:12:01 +0100

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp, getApps, getApp } from 'firebase/app'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBertalx96gHldekbEspu2RdpcqW1iH7x8",
@@ -15,4 +16,5 @@ const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp()
 
 export const auth = getAuth(app)
 export const db = getFirestore(app)
+export const storage = getStorage(app)
 export const googleProvider = new GoogleAuthProvider()
