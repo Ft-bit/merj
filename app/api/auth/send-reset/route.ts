3d7368267e7f4c0b2315b021ef54a6c01c505856
Ft.dev <fredbv22@gmail.com>
2026-09-13 21:58:44 +0100

    Update route.ts

--- app/api/auth/send-reset/route.ts
+++ app/api/auth/send-reset/route.ts
@@ -13,8 +13,7 @@ export async function POST(req: Request) {
 
   try {
     const link = await adminAuth.generatePasswordResetLink(email, {
-      url: 'https://merj-seven.vercel.app/auth/action',
-      handleCodeInApp: true,
+      url: 'https://merj-seven.vercel.app/auth/action?type=reset',
     })
 
     await transporter.sendMail({
