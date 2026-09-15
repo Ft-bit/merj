528517f75d9ed2959d1337a2b5a92fee551722be
Ft.dev <fredbv22@gmail.com>
2026-09-15 07:54:20 +0100

    Update firebaseAdmin.ts

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,5 +1,6 @@
 import { initializeApp, getApps, cert } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
+import { getFirestore } from 'firebase-admin/firestore'
 
 const app = getApps().length
   ? getApps()[0]
@@ -12,3 +13,4 @@ const app = getApps().length
     })
 
 export const adminAuth = getAuth(app)
+export const adminDb = getFirestore(app)
